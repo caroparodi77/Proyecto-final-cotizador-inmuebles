b4f1a5fccc0fcfc7083364dcc664b1012637050e
Carolina Parodi <caromparodi77@gmail.com>
2023-11-04 16:12:07 -0300

    organice las lineas de codigo para que sea mas legible

--- src/componentes/Historial.jsx
+++ src/componentes/Historial.jsx
@@ -1,56 +1,56 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
 function Historial() {
-    const [historialCotizaciones, setHistorialCotizaciones] = useState([]);
-    const historial = JSON.parse(
-        window.localStorage.getItem("historial") || "[]"
-      );
+  const [historialCotizaciones, setHistorialCotizaciones] = useState([]);
+  const historial = JSON.parse(
+    window.localStorage.getItem("historial") || "[]"
+  );
 
-    useEffect(() => {
-        const cargarHistorial = () => {
-        const historialGuardado = JSON.parse(localStorage.getItem("historialCotizaciones")) || [];
-        setHistorialCotizaciones(historialGuardado);
+  useEffect(() => {
+    const cargarHistorial = () => {
+      const historialGuardado =
+        JSON.parse(localStorage.getItem("historialCotizaciones")) || [];
+      setHistorialCotizaciones(historialGuardado);
     };
 
     cargarHistorial();
-    }, []);
+  }, []);
 
-    const retornoTablaHTML = (fila) => {
-        return (
-            <tr key={fila.fecha}>
-                <td>{fila.fecha}</td>
-                <td>{fila.propiedad}</td>
-                <td>{fila.ubicacion}</td>
-                <td>{fila.metros2}</td>
-                <td>$ {fila.cotizacion}</td>
-            </tr>
-        );
-    };
-
+  const retornoTablaHTML = (fila) => {
     return (
-        <>
-        <table className='table table-hover'>
-            <thead>
-            <tr>
-                <th>Fecha de cotización</th>
-                <th>Propiedad</th>
-                <th>Ubicación</th>
-                <th>Metros cuadrados</th>
-                <th>Póliza mensual</th>
-            </tr>
-            </thead>
-     
-        <tbody>
-            {historial.map((fila) => retornoTablaHTML(fila))}
-            </tbody>
-            </table>
-            <div className="container-button">
-                <Link to="/"><button className="button button-outline">VOLVER</button></Link>
-            </div>
-        </>
+      <tr key={fila.fecha}>
+        <td>{fila.fecha}</td>
+        <td>{fila.propiedad}</td>
+        <td>{fila.ubicacion}</td>
+        <td>{fila.metros2}</td>
+        <td>$ {fila.cotizacion}</td>
+      </tr>
     );
+  };
+
+  return (
+    <>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Fecha de cotización</th>
+            <th>Propiedad</th>
+            <th>Ubicación</th>
+            <th>Metros cuadrados</th>
+            <th>Póliza mensual</th>
+          </tr>
+        </thead>
+
+        <tbody>{historial.map((fila) => retornoTablaHTML(fila))}</tbody>
+      </table>
+      <div className="container-button">
+        <Link to="/">
+          <button className="button button-outline">VOLVER</button>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default Historial;
--- src/componentes/InputMetros.jsx
+++ src/componentes/InputMetros.jsx
@@ -1,17 +1,19 @@
-export function InputMetros({handleChange}) {
-
-
-return (
-
-<div className="col-md">
-            <div className="form-floating">
-                <div>
-                    <label htmlFor="metros2">Ingresa los metros cuadrados</label>
-                </div>
-                    <input className= "input" type="number" id="metros2" onChange={handleChange}
-                    min="20" required
-                    />
-            </div>
+export function InputMetros({ handleChange }) {
+  return (
+    <div className="col-md">
+      <div className="form-floating">
+        <div>
+          <label htmlFor="metros2">Ingresa los metros cuadrados</label>
         </div>
-         )  
-};       
+        <input
+          className="input"
+          type="number"
+          id="metros2"
+          onChange={handleChange}
+          min="20"
+          required
+        />
+      </div>
+    </div>
+  );
+}
